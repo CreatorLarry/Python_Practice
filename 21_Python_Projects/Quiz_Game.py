42bbde31861b4ca97ce4ff5584090b83c4769da7
Larry <lawrencengugi90@gmail.com>
2024-11-14 06:44:09 +0300

    Quiz game project, COMPLETE!

--- 21_Python_Projects/Quiz_Game.py
+++ 21_Python_Projects/Quiz_Game.py
@@ -6,7 +6,7 @@ if playing.lower() != "yes":
     quit()
 print("Okay! Lets play ; ")
 
-print("1. 5 Wed-Dev Acronyms \n2. 6 Proverbs with Larry \n3. 5 Questions You should know answers to")
+print("1. 5 Wed-Dev Acronyms \n2. 5 Proverbs with Larry \n3. 5 Questions Trivia")
 
 game_play = input("Enter Number of Game you'd like to play :) ")
 # keeping score
@@ -88,49 +88,62 @@ elif game_play == "2":
     else:
         print("Incorrect! \n correct answer is:  \033[1mLook before\033[0m you leap.")
 
-    answer = input("Every cloud ").lower()
-    if answer == "has a silver lining":
-        print("Correct!")
-        score += 1
-    else:
-        print("Incorrect! \n correct answer is:  \033[1mEvery cloud \033[0m has a silver lining.")
+    # answer = input("Every cloud ").lower()
+    # if answer == "has a silver lining":
+    #     print("Correct!")
+    #     score += 1
+    # else:
+    #     print("Incorrect! \n correct answer is:  \033[1mEvery cloud \033[0m has a silver lining.")
 
 # Game 3
 elif game_play == "3":
-    answer = input("The early bird ").lower()
-    if answer == "catches the worm":
+    print("Welcome to the 5 Question Trivia \nAll the best.")
+    answer = input("What is James Bond's code name? ").lower()
+    if answer == "007":
         print("Correct!")
         score += 1
     else:
-        print("Incorrect! \n correct answer is: \033[1mThe early bird\033[0m catches the worm.")
+        print("Incorrect! \n\033[Correct answer:\033[0m 007.")
 
-    answer = input("Cleanliness is ").lower()
-    if answer == "next to Godliness":
+    answer = input("According to Guinness World Records, what's the best-selling book of all time?  The").lower()
+    if answer == "bible":
         print("Correct! ")
         score += 1
     else:
-        print("Incorrect! \n correct answer is: \033[Cleanliness is\033[0m next to Godliness.")
+        print("Incorrect! \n\033[Correct answer:\033[0m The bible.")
 
-    answer = input("Strike while ").lower()
-    if answer == "the iron is hot":
+    answer = input("Which soft drink once contained cocaine as one of its original ingredients? ").lower()
+    if answer == "coca-cola":
         print("Correct! ")
         score += 1
     else:
-        print("Incorrect! \n correct answer is: \033[1mStrike while\033[0m the iron is hot.")
+        print("Incorrect! \n\033[Correct answer:\033[0m coca-cola.")
 
-    answer = input("Don't count your chickens ").lower()
-    if answer == "before they hatch":
+    answer = input("Which travels faster: Sound or light? ").lower()
+    if answer == "light":
         print("Correct!")
         score += 1
     else:
-        print("Incorrect! \n correct answer is: \033[1mDon't count your chicken\033[0m before they hatch.")
+        print("Incorrect! \n\033[Correct answer:\033[0m Light")
 
-    answer = input("Look before ").lower()
-    if answer == "you leap":
+    answer = input("Which blood type is a universal donor? ").lower()
+    if answer == "o negative":
         print("Correct!")
         score += 1
     else:
-        print("Incorrect! \n correct answer is:  \033[1mLook before\033[0m you leap.")
+        print("Incorrect! \n\033[Correct answer:\033[0m o negative")
+
+else:
+    print("Number between 1 and 3")
+
 # printing score
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score / 5) * 100) + " %.")
+
+if 2 <= score >= 0:
+    print("You can do better")
+elif 4 <= score >= 3:
+    print("Average")
+elif 5 == score >= 4:
+    print("Genius! ;) Treat on Me.")
+
